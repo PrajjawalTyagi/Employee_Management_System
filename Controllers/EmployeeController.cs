bffe48ef5e96cd68cf4298c41b20d4edba323776
Prajjawal_Tyagi <Prajjawaltyagi87@gmail.com>
2026-05-16 18:12:21 +0530

    Working fin with add swagger

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,23 +1,15 @@
-﻿using Employee_Management_system.Model;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Employee_Management_system.Controllers
 {
-    public class EmployeeController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
     {
-         private readonly Employee_DbContext _context;
-        public EmployeeController(Employee_DbContext context)
-        {
-            _context = context;
-        }
-        public IActionResult Index()
-        {
-            return View();
-        }
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult Get()
         {
-            return Ok();
-        } 
+            return Ok("API Working");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,24 +1,34 @@
 using Employee_Management_system.Model;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-//for connectiion register Db sql server..
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Employee API",
+        Version = "v1"
+    });
+});
+
 builder.Services.AddDbContext<Employee_DbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
 {
-    app.MapOpenApi();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee API V1");
+});
 
 app.UseHttpsRedirection();
 
