f46b47c8210179bcb98396a7ef1e369247d3cb4b
Prajjawal_Tyagi <Prajjawaltyagi87@gmail.com>
2026-05-16 18:40:38 +0530

    Crud operations done

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Employee_Management_system.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Employee_Management_system.Controllers
 {
@@ -6,10 +7,65 @@ namespace Employee_Management_system.Controllers
     [Route("api/[controller]")]
     public class EmployeeController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
+        private readonly Employee_DbContext _context;
+
+        public EmployeeController( Employee_DbContext context)
         {
-            return Ok("API Working");
+            _context = context;
+        }
+
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    return Ok("API Working");
+        //}
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return _context.Employees != null ? Ok(_context.Employees.ToList()) : NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult EmployeeData( Employee employee)
+        {
+            if(employee == null)
+                return BadRequest("Employee data is null.");
+            var newEmployee = new Employee
+            {
+                EmployeeName = employee.EmployeeName,
+                EmployeeEmail = employee.EmployeeEmail,
+                CreatedDate = DateTime.Now
+            };
+            _context.Employees.Add(newEmployee);
+            _context.SaveChanges();
+            return Ok(newEmployee);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateEmployee(int id, Employee employee)
+        {
+            var existingEmployee = _context.Employees.Find(id);
+            if (existingEmployee == null)
+                return NotFound("Employee not found.");
+            existingEmployee.EmployeeName = employee.EmployeeName;
+            existingEmployee.EmployeeEmail = employee.EmployeeEmail;
+            _context.SaveChanges();
+            return Ok(existingEmployee);
+        }
+
+        [HttpDelete]
+        public IActionResult Employee_Delete( int id)
+        {
+            if (id == null)
+                return BadRequest( new { message="Not found"});
+            var result = _context.Employees.FirstOrDefault(x => x.EmployeeId == id);
+            if (result is null)
+                return NotFound("Id not found in  Database");
+            _context.Employees.Remove(result);
+            _context.SaveChanges();
+            return Ok();
         }
     }
+
 }
